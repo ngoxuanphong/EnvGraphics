7dbfaf4ee39d438b953b01e9cb7fda00c2d25929
NgoXuanPhong <xuanphongcht@gmail.com>
2023-04-10 04:30:21 +0700

    add đo hoa sushigo

--- Base/SushiGo/_env.py
+++ Base/SushiGo/_env.py
@@ -3,7 +3,7 @@ import random
 
 from numba import njit
 
-# njit()
+@njit()
 def initEnv(n):
     '''
     n : số lượng người chơi\n
@@ -40,7 +40,7 @@ def initEnv(n):
         state_sys =  np.concatenate((state_sys ,np.array([0,0]) ,np.array([-1 for i in range(12-n)])))
     return state_sys
 
-# njit()
+@njit()
 def getAgentState(state_sys,index_player):
     amount_player = 5
     round = state_sys[0] - 1
@@ -76,7 +76,7 @@ def getAgentState(state_sys,index_player):
             step += 12
     return state_player.astype(np.float64)
 
-# njit()
+@njit()
 def caculater_for_one(card):
     score_dumpling = [0,1,3,6,10,15]
     score = card[0]
@@ -114,18 +114,18 @@ def caculater_for_one(card):
 
     return score,np.count_nonzero(card ==9)
 
-# njit()
+@njit()
 def count_maki(card):
     maki_1 = np.count_nonzero(card == 3)
     maki_2 = np.count_nonzero(card == 4)
     maki_3 = np.count_nonzero(card == 5)
     return maki_1 + maki_2*2 + maki_3*3
 
-# njit()
+@njit()
 def get_index(arr,first,second):
     return np.where(arr == first)[0],np.where(arr == second)[0]
 
-# njit()
+@njit()
 def caculator_pudding(state_sys,amount_player):
     amount_player = state_sys[2]
     arr_pudding = np.array([0 for i in range(amount_player)])
@@ -144,7 +144,7 @@ def caculator_pudding(state_sys,amount_player):
             state_sys[index_start_player_s] += score
     return state_sys
 
-# njit()
+@njit()
 def caculater_score(state_sys,amount_player):
     amount_player = state_sys[2]
     round = state_sys[0] - 1
@@ -177,7 +177,7 @@ def caculater_score(state_sys,amount_player):
                 break
     return state_sys
 
-# njit()
+@njit()
 def getReward(state_player):
     if state_player[1] <= 21:
         return -1
@@ -199,7 +199,7 @@ def getReward(state_player):
     else:
         return 0
 
-# njit()
+@njit()
 def winner_victory(state_sys):
     amount_player = int(state_sys[2])
     list_score = state_sys[3+3*amount_player*7::14-amount_player]
@@ -216,7 +216,7 @@ def winner_victory(state_sys):
         return winner[winner_puding]
 
 
-# njit()
+@njit()
 def stepEnv(state_sys,list_action,amount_player,turn,round):
     player = 0
     turn +=4
@@ -239,7 +239,7 @@ def stepEnv(state_sys,list_action,amount_player,turn,round):
     return state_sys
 
 
-# njit()
+@njit()
 def reset_card_player(state_sys):
     amount_player = state_sys[2]
     for player in range(amount_player):
@@ -250,7 +250,7 @@ def reset_card_player(state_sys):
     return state_sys
 
 
-# njit()
+@njit()
 def test_action(player_state,action):
     index_between = 14
     if action == 12:
@@ -266,13 +266,13 @@ def test_action(player_state,action):
         player_state[-2] -= 1
     return player_state
 
-# njit()
+@njit()
 def move_card(state,card,start_1 = 0,start_2 = 0):
     state[start_1+card] -= 1
     state[start_2+card] += 1
     return state
 
-# njit()
+@njit()
 def move_card_step(state,card,start_1 = 0,end_1=0,start_2 = 0,end_2 = 0):
     index_relative_from = np.where(state[start_1:end_1] == card)[0]
     index_relative_to = np.where(state[start_2:end_2]==-1)[0]
@@ -364,21 +364,21 @@ def one_game(list_player,per_file):
     winner = winner_victory(state_sys)
     return winner,per_file
 
-# njit()
+@njit()
 def getActionSize():
     return 14
 
-# njit()
+@njit()
 def getAgentSize():
     return 5
 
-# njit()
+@njit()
 def getStateSize():
     return 86
 
 
 
-# njit()
+@njit()
 def getValidActions(player_state_origin:np.int64):
     list_action_return = np.zeros(14)
     player_state = player_state_origin.copy()
@@ -432,7 +432,7 @@ def normal_main_print(list_player,amount_game,file_per):
 
 
 
-# njit()
+@njit()
 def one_game_numba(p0, list_other, per_player, per1, per2, per3, per4, p1, p2, p3, p4):
     amount_player = 5
     state_sys = initEnv(amount_player)
@@ -495,7 +495,7 @@ def one_game_numba(p0, list_other, per_player, per1, per2, per3, per4, p1, p2, p
     else: result = 0
     return result,per_player
 
-# njit()
+@njit()
 def n_games_numba(p0, num_game, per_player, list_other, per1, per2, per3, per4, p1, p2, p3, p4):
     win = 0
     for _ in range(num_game):
@@ -595,14 +595,14 @@ def load_module_player(player, game_name = None):
     return module
 
 
-# njit()
+@njit()
 def bot_lv0(state, perData):
     validActions = getValidActions(state)
     arr_action = np.where(validActions==1)[0]
     idx = np.random.randint(0, arr_action.shape[0])
     return arr_action[idx], perData
 
-# njit()
+@njit()
 def check_run_under_njit(agent, perData):
     return True
 
