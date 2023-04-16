ab9926caf3fc19df23db8dc31bc1aa85c5df1b9d
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2023-04-16 22:02:03 +0700

    stoneage sửa không cho dùng action 47 lúc mua thẻ build 1-7

--- Base/StoneAge/_env.py
+++ Base/StoneAge/_env.py
@@ -418,7 +418,6 @@ def getValidActions(p_state):
 
     if phase == 3: #Trả nguyên liệu mua thẻ civ
         list_action[40:44] = (p_state[s_ + 5:s_ + 9] > 0)
-        list_action[47] = 1
 
     if phase == 4: #Dùng công cụ(end hoặc hết thì qua roll xúc xắc)
         list_action[37:40] = (p_state[s_ + 15:s_ + 18] > 0)
@@ -427,6 +426,7 @@ def getValidActions(p_state):
 
     if phase == 5: #trả nguyên liệu khi mua thẻ build 1 -> 7
         list_action[40:44] = (p_state[s_ + 5:s_ + 9] > 0)
+        list_action[47] = 1
 
     if phase == 6: #Chọn trừ nguyên liệu hoặc trừ điểm khi không đủ thức ăn
         list_action[27] = 1
